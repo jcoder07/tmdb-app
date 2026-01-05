d7654ad42ac7d7757ae5f9236176b4eb617f3038
Juan Fernandez <jjfernandezm17@gmail.com>
2026-01-05 13:57:43 -0300

    Initial commit

---
+++ tmdb-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tmdb-app
+//
+//  Created by Juan Fernandez on 05-01-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ tmdb-appTests/tmdb_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  tmdb_appTests.swift
+//  tmdb-appTests
+//
+//  Created by Juan Fernandez on 05-01-26.
+//
+
+import Testing
+@testable import tmdb_app
+
+struct tmdb_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
