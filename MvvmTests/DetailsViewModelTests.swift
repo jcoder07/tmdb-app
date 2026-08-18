e76eb8efcb90a1e5dd7f5be99f0330eafcafcf44
Juan Fernandez <jjfernandezm17@gmail.com>
2026-08-18 18:07:23 -0400

    Add comment to tests

--- MvvmTests/DetailsViewModelTests.swift
+++ MvvmTests/DetailsViewModelTests.swift
@@ -11,35 +11,41 @@ import Testing
 struct DetailsViewModelTests {
 
     @Test func testSum() {
+        // Arrange
         let sut: DetailsViewModel = makeSUT()
         
+        // Act
         sut.numberOne = "10"
         sut.numberTwo = "20"
-        
         sut.sum()
         
+        // Assert
         #expect(sut.result == 30)
     }
     
     @Test func testSumWithInitialValueZero() {
+        // Arrange
         let sut: DetailsViewModel = makeSUT()
         
+        // Act
         sut.numberOne = "t"
         sut.numberTwo = "j"
-        
         sut.sum()
         
+        // Assert
         #expect(sut.result == 0)
     }
     
     @Test func testFormattedResult() {
+        // Arrange
         let sut: DetailsViewModel = makeSUT()
         
+        // Act
         sut.numberOne = "7"
         sut.numberTwo = "7"
-        
         sut.sum()
         
+        // Assert
         #expect(sut.formattedResult == "14")
     }
     
