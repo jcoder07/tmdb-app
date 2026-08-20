2309149065e51fd9628a9a3b3cc379d67ec37b3f
Juan Fernandez <jjfernandezm17@gmail.com>
2026-08-20 16:57:53 -0400

    Reorganize Mock Class

--- Mvvm/Details/DetailsView.swift
+++ Mvvm/Details/DetailsView.swift
@@ -28,27 +28,6 @@ struct DetailsView: View {
     }
 }
 
-class MockDetails {
-    
-    class MockMovieDetaolRepository: MovieDetailRepository {
-        
-        func getMovieDetails() async throws -> Movie? {
-            
-            let movie = Movie(originalTitle: "Test2", overview: "Test2", popularity: 20)
-            
-            return movie
-        }
-
-    }
-    
-    static func makeDetailsViewModel() -> DetailsViewModel {
-        let viewModel = DetailsViewModel(repository: MockMovieDetaolRepository())
-        
-        viewModel.movie = .init(originalTitle: "Test", overview: "Test", popularity: 10)
-        return viewModel
-    }
-    
-}
 
 #Preview("Details-View") {
     
---
+++ Mvvm/Details/Helper/MockDetails.swift
@@ -0,0 +1,30 @@
+//
+//  MockDetails.swift
+//  Mvvm
+//
+//  Created by Juan Fernandez on 20-08-26.
+//
+
+import Foundation
+
+class MockDetails {
+    
+    class MockMovieDetaolRepository: MovieDetailRepository {
+        
+        func getMovieDetails() async throws -> Movie? {
+            
+            let movie = Movie(originalTitle: "Test2", overview: "Test2", popularity: 20)
+            
+            return movie
+        }
+
+    }
+    
+    static func makeDetailsViewModel() -> DetailsViewModel {
+        let viewModel = DetailsViewModel(repository: MockMovieDetaolRepository())
+        
+        viewModel.movie = .init(originalTitle: "Test", overview: "Test", popularity: 10)
+        return viewModel
+    }
+    
+}
